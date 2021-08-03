f7c19a634d8b8d56bfe09df2122cdcfb9e2a6d80
Rafal <rafal1213@gmail.com>
2021-08-03 19:45:29 +0200

    naming

--- src/test/java/com/futurecollars/MapExampleTest.java
+++ src/test/java/com/futurecollars/MapExampleTest.java
@@ -5,56 +5,74 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MapExampleTest
 {
 
-    //unikalne klucze
+    //unikalne klucze, ale wartosci moga sie powtarzac
     @Test
-    void shouldCreateMap2() {
+    void unique()
+    {
         Map<Long, String> names = new HashMap<>();
         names.put(1L, "Kamil");
         System.out.println("Mapa przed nadpisaniem: " + names);
         names.put(1L, "Pablo");
         System.out.println("Mapa po nadpisaniu: " + names);
+
+        Assertions.assertEquals(2, names.size());
     }
 
     //iterowanie
     @Test
-    void shouldCreateMap() {
+    void iterate()
+    {
         Map<Long, String> countries = new HashMap<>();
 
         countries.put(1L, "Poland");
         countries.put(2L, "Germany");
 
-        for(Long key : countries.keySet()) {
+        for (Long key : countries.keySet())
+        {
             System.out.println("Key: " + key + ", value: " + countries.get(key));
         }
 
-        for(Map.Entry<Long, String> entry : countries.entrySet()) {
+        for (Map.Entry<Long, String> entry : countries.entrySet())
+        {
             System.out.println("Key: " + entry.getKey() + ", value: " + entry.getValue());
         }
     }
 
 
-
     //jezeli zalezy Ci na kolejnosci to LinkedHashMap
     @Test
-    void shouldCreateMap3() {
-        Map<Person, Integer> orderMap = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            orderMap.put(new Person("Test" + i), i);
+    void iterate2()
+    {
+        Map<Person, Integer> people = new HashMap<>();
+        for (int i = 0; i < 10; i++)
+        {
+            people.put(new Person("Test" + i), i);
         }
-        System.out.println("Order map: " + orderMap);
+        System.out.println("Order map: " + people);
 
 
-        for (int i = 10; i < 20; i++) {
-            orderMap.put(new Person("Test" + i), i);
+        for (int i = 10; i < 20; i++)
+        {
+            people.put(new Person("Test" + i), i);
         }
-        System.out.println("Order map: " + orderMap);
+        System.out.println("Order map: " + people);
+    }
+
+    //dla nulli jest specjalny bucket bo jak na nullu wywolac hashcode - zerowy bucket
+    @Test
+    void nullValues()
+    {
+        Map<Person, Person> people = new HashMap<>();
+        Person rafal = new Person("Rafal");
+        people.put(null, rafal);
+        Person kasia = new Person("Kasia");
+        people.put(null, kasia);
+
+        System.out.println(people);
     }
 
     //klucze niemodyfikowalne
